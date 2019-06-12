301bce9cdaafafbd85ba9d1b4d519ac41fb6379a
Lê Như Phương <43329732+Lephuong96@users.noreply.github.com>
2019-06-12 14:46:54 +0700

    update ArrayListTest

--- ArrayListTest.php
+++ ArrayListTest.php
@@ -1,9 +1,16 @@
 <?php
 include "ArrayList.php";
 
+
 $listInteger = new ArrayList();
 $listInteger->add(1);
 $listInteger->add(2);
 $listInteger->add(3);
-
-echo $listInteger->get(2);
+$listInteger->add(4);
+$listInteger->add(5);
+$listInteger->add(6);
+$listInteger->add(7);
+$listInteger->remove(3);
+var_dump($listInteger);
+echo "</br>";
+echo $listInteger->get(5);
